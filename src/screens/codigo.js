39d5bc745717d57af24f5fd92fe86ef5d31388c2
abigc23 <rociogutierrezcao@gmail.com>
2024-10-03 18:32:53 -0300

    Estilo final de crear receta

--- src/screens/codigo.js
+++ src/screens/codigo.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, TextInput, TouchableOpacity, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import Header from '../components/Headers/Header';
 import * as ImagePicker from 'expo-image-picker'; 
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'; 
 import { storage } from '../../firebase/firebase-config';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Picker } from '@react-native-picker/picker';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../../firebase/firebase-config'; // Ensure you import db from your firebase-config
 
 const CreateRecipeScreen = () => {
   const [activeTab, setActiveTab] = useState('ingredients');
@@ -19,84 +17,25 @@ const CreateRecipeScreen = () => {
   const [glutenFree, setGlutenFree] = useState('no'); 
   const [vegetarian, setVegetarian] = useState('no'); 
   const [servings, setServings] = useState(''); 
-  const [prepTime, setPrepTime] = useState('');
-  const [categories, setCategories] = useState([]); 
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [loading, setLoading] = useState(true); // State to manage loading status
+  const [prepTime, setPrepTime] = useState(''); 
 
-  const fetchCategories = async () => {
-    try {
-      const categoriesCollection = collection(db, 'categories'); 
-      const snapshot = await getDocs(categoriesCollection);
-      const fetchedCategories = snapshot.docs.map(doc => doc.data().name);
-      setCategories(fetchedCategories);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching categories:', error);
-      Alert.alert('Error', 'No se pudieron cargar las categorías. Inténtalo de nuevo más tarde.');
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCategories();
-  }, []);
-
-  const requestPermissions = async () => {
-    const { status: mediaLibraryStatus } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (mediaLibraryStatus !== 'granted') {
-      Alert.alert('Permiso requerido', 'Lo sentimos, necesitamos permisos para acceder a la galería.');
-      return false;
-    }
-    return true;
-  };
-
-  // Function to pick an image
   const pickImage = async () => {
-    const hasPermission = await requestPermissions();
-    if (!hasPermission) return;
-
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (result.assets && result.assets.length > 0) {
-      const selectedImage = result.assets[0];
-      const response = await fetch(selectedImage.uri);
-      const blob = await response.blob();
-
-      const filename = selectedImage.uri.substring(selectedImage.uri.lastIndexOf('/') + 1);
-      const imageRef = ref(storage, `recipeImages/${filename}`); 
-
-      try {
-        await uploadBytes(imageRef, blob);
-        const url = await getDownloadURL(imageRef);
-        setRecipeImage(url); 
-      } catch (error) {
-        console.error('Error al subir la imagen:', error);
-        Alert.alert('Error', 'No se pudo subir la imagen.'); 
-      }
-    }
+    // Implementación para seleccionar imagen
   };
 
   const saveRecipe = async () => {
-    Alert.alert(`Receta "${recipeName}" guardada con éxito.`);
+    alert(`Receta "${recipeName}" guardada con éxito.`);
     console.log({
-      recipeName,
-      recipeImage,
-      ingredients,
-      preparation,
-      dietType,
-      glutenFree,
-      vegetarian,
-      prepTime,
-      servings,
-      category: selectedCategory,
+        recipeName,
+        recipeImage,
+        ingredients,
+        preparation,
+        dietType,
+        glutenFree,
+        vegetarian,
+        prepTime,
+        servings,
     });
-    
   };
 
   const handleAddIngredient = () => {
@@ -152,47 +91,32 @@ const CreateRecipeScreen = () => {
             onChangeText={setRecipeName}
           />
 
-<View>
-            <View style={styles.row}>
-              <View style={styles.iconInputWrapper}>
-                <Icon name="people-outline" size={24} color="#333" />
-                <TextInput
-                  style={styles.inputDetail}
-                  placeholder="Porciones"
-                  value={servings}
-                  onChangeText={setServings}
-                />
-              </View>
-              <View style={styles.iconInputWrapper}>
-                <Icon name="time-outline" size={24} color="#333" />
-                <TextInput
-                  style={styles.inputDetail}
-                  placeholder="Tiempo de preparación"
-                  value={prepTime}
-                  onChangeText={setPrepTime}
-                  multiline={true}
-                />
-              </View>
-            </View>
-
+          <View style={styles.row}>
             <View style={styles.iconInputWrapper}>
-              <Icon name="list-outline" size={24} color="#333" />
-              <Picker
-                selectedValue={selectedCategory}
+              <Icon name="people-outline" size={24} color="#333" />
+              <TextInput
                 style={styles.inputDetail}
-                onValueChange={(itemValue) => setSelectedCategory(itemValue)}
-              >
-                <Picker.Item label="Seleccione una categoría" value="" />
-                {categories.map((category, index) => (
-                  <Picker.Item key={index} label={category} value={category} />
-                ))}
-              </Picker>
+                placeholder="Porciones"
+                value={servings}
+                onChangeText={setServings}
+              />
+            </View>
+            <View style={styles.iconInputWrapper}>
+              <Icon name="time-outline" size={24} color="#333" />
+              <TextInput
+                style={styles.inputDetail}
+                placeholder="Tiempo de preparación"
+                value={prepTime}
+                onChangeText={setPrepTime}
+                multiline={true}
+              />
             </View>
           </View>
 
-
+          {/* Línea separadora */}
           <View style={styles.separator} />
 
+          {/* Tabs de Ingredientes, Preparación y Dietas */}
           <View style={styles.tabsContainer}>
             <TouchableOpacity
               style={[styles.tabButton, activeTab === 'ingredients' ? styles.activeTab : styles.inactiveTab]}
@@ -214,6 +138,7 @@ const CreateRecipeScreen = () => {
             </TouchableOpacity>
           </View>
 
+          {/* Renderizar contenido según la pestaña activa */}
           {activeTab === 'ingredients' && (
             <View style={styles.ingredientsContainer}>
               {ingredients.map((ingredient, index) => (
@@ -263,27 +188,31 @@ const CreateRecipeScreen = () => {
                 <Picker
                   selectedValue={dietType}
                   onValueChange={(itemValue) => setDietType(itemValue)}
+                  style={styles.picker}
                 >
-                  <Picker.Item label="Vegana" value="vegan" />
-                  <Picker.Item label="Vegetariana" value="vegetarian" />
-                  <Picker.Item label="Sin gluten" value="glutenFree" />
+                  <Picker.Item label="Vegano" value="vegan" />
+                  <Picker.Item label="No Vegano" value="non-vegan" />
                 </Picker>
               </View>
+
               <View style={styles.pickerContainer}>
-                <Text style={styles.pickerLabel}>¿Sin gluten?</Text>
+                <Text style={styles.pickerLabel}>Sin TACC:</Text>
                 <Picker
                   selectedValue={glutenFree}
                   onValueChange={(itemValue) => setGlutenFree(itemValue)}
+                  style={styles.picker}
                 >
                   <Picker.Item label="Sí" value="yes" />
                   <Picker.Item label="No" value="no" />
                 </Picker>
               </View>
+
               <View style={styles.pickerContainer}>
-                <Text style={styles.pickerLabel}>¿Vegetariano?</Text>
+                <Text style={styles.pickerLabel}>Vegetariano:</Text>
                 <Picker
                   selectedValue={vegetarian}
                   onValueChange={(itemValue) => setVegetarian(itemValue)}
+                  style={styles.picker}
                 >
                   <Picker.Item label="Sí" value="yes" />
                   <Picker.Item label="No" value="no" />
@@ -291,11 +220,11 @@ const CreateRecipeScreen = () => {
               </View>
             </View>
           )}
-        </View>
 
-        <TouchableOpacity style={styles.saveButton} onPress={saveRecipe}>
-          <Text style={styles.saveButtonText}>Guardar receta</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={saveRecipe} style={styles.saveButton}>
+            <Text style={styles.saveButtonText}>Guardar Receta</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </>
   );
@@ -328,69 +257,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
-    marginTop: -30,
     padding: 20,
+    marginTop: -40,
   },
   recipeName: {
     fontSize: 24,
-    textAlign: 'left',
-    marginVertical: 40,
-  },
-  input: {
-    backgroundColor: '#f1f1f1',
-    borderRadius: 30,
-    padding: 15,
+    fontWeight: 'bold',
     marginBottom: 20,
-    fontSize: 16,
-    height: 50,
-    paddingLeft: 20,
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     marginBottom: 20,
   },
   iconInputWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    flex: 1,
-    marginHorizontal: 35,
-    paddingBottom: 5,
-    borderRadius: 30,
-    height: 50,
   },
   inputDetail: {
-    flex: 1,
-    fontSize: 16,
-    marginLeft: 10,
-    backgroundColor: 'transparent',
-    width: '30%',
-    lineHeight: 20,
-    height: 'auto',
-    textAlignVertical: 'top',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingLeft: 10,
+    marginLeft: 5,
+    width: 100,
   },
-  tabsDietas: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginBottom: 20,
-    borderRadius: 25,
-    backgroundColor: '#f5f5f5',
-    padding: 5,
-    width: '',
-  },
-  ButtonDieta: {
-    flex: 1,
-    paddingVertical: 10,
-    alignItems: 'center',
+  separator: {
+    height: 1,
+    backgroundColor: '#ccc',
+    marginVertical: 20,
   },
   tabsContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     marginBottom: 20,
-    borderRadius: 25,
-    backgroundColor: '#f5f5f5',
-    padding: 5,
   },
   tabButton: {
     flex: 1,
@@ -398,25 +297,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   activeTab: {
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    paddingHorizontal: 15,
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
   },
   inactiveTab: {
-    backgroundColor: 'transparent',
+    borderBottomWidth: 2,
+    borderBottomColor: 'transparent',
   },
   tabText: {
     fontSize: 16,
-    color: '#888',
   },
   activeTabText: {
-    color: '#000',
     fontWeight: 'bold',
   },
   ingredientsContainer: {
-    marginBottom: 20,
-  },
-  preparationContainer: {
     marginBottom: 20,
   },
   ingredientRow: {
@@ -426,50 +320,51 @@ const styles = StyleSheet.create({
   },
   textArea: {
     flex: 1,
-    backgroundColor: '#f1f1f1',
-    borderRadius: 30,
-    padding: 15,
-    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
     marginRight: 10,
   },
   addButton: {
+    backgroundColor: '#4CAF50',
     padding: 10,
+    borderRadius: 5,
     alignItems: 'center',
   },
   addButtonText: {
-    color: '#008CBA',
-    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  preparationContainer: {
+    marginBottom: 20,
+  },
+  dietTypeContainer: {
+    marginBottom: 20,
+  },
+  pickerContainer: {
+    marginBottom: 20,
+  },
+  pickerLabel: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  picker: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
   },
   saveButton: {
-    backgroundColor: '#e86900',
-    borderRadius: 30,
+    backgroundColor: '#4CAF50',
     padding: 15,
+    borderRadius: 5,
     alignItems: 'center',
-    width: '80%',
-    justifyContent:'center',
-    marginVertical: 15,
-    margin:'auto',
   },
   saveButtonText: {
     color: '#fff',
-    fontSize: 16,
     fontWeight: 'bold',
-  },
-  pickerContainer: {
-    marginBottom: 16,
-  },
-  pickerLabel: {
     fontSize: 16,
-    marginBottom: 8,
-  },
-  picker: {
-    height: 50,
-    width: '100%',
-  },
-  separator: {
-    height: 1,
-    backgroundColor: '#ddd',
-    marginVertical: 16,
   },
 });
 
