dc766207686704644c9791050ec52ee07be70599
Aleu79 <aleeaguero79@gmail.com>
2024-10-18 00:27:21 -0300

    correcciones 3.5

--- src/screens/CreateRecipeScreen.js
+++ src/screens/CreateRecipeScreen.js
@@ -6,7 +6,7 @@ import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../firebase/firebase-config';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Picker } from '@react-native-picker/picker';
-import { collection, getDocs, doc, updateDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 import { db } from '../../firebase/firebase-config'; 
 import { auth } from '../../firebase/firebase-config'; 
 
@@ -21,28 +21,9 @@ const CreateRecipeScreen = () => {
   const [vegetarian, setVegetarian] = useState('no'); 
   const [servings, setServings] = useState(''); 
   const [prepTime, setPrepTime] = useState('');
-  const [categories, setCategories] = useState([]); 
+  const [categories] = useState(['Sin TACC', 'Sin lácteos', 'Vegetariano', 'Vegano']); 
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [loading, setLoading] = useState(true); 
-
-  // Fetch categories from Firestore
-  const fetchCategories = async () => {
-    try {
-      const categoriesCollection = collection(db, 'categories'); 
-      const snapshot = await getDocs(categoriesCollection);
-      const fetchedCategories = snapshot.docs.map(doc => doc.data().name);
-      setCategories(fetchedCategories);
-    } catch (error) {
-      console.error('Error fetching categories:', error);
-      Alert.alert('Error', 'No se pudieron cargar las categorías. Inténtalo de nuevo más tarde.');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCategories();
-  }, []);
+ 
 
   // Request media library permissions
   const requestPermissions = async () => {
@@ -53,7 +34,14 @@ const CreateRecipeScreen = () => {
     }
     return true;
   };
-
+  let recipeCounter = 0;
+  const generateId = () => {
+    const timestamp = Date.now();
+    const randomNum = Math.floor(Math.random() * 1000); 
+    return `recipe_${timestamp}_${randomNum}`;
+  };
+  
+  
   // Pick an image from the gallery
   const pickImage = async () => {
     const hasPermission = await requestPermissions();
@@ -75,22 +63,31 @@ const CreateRecipeScreen = () => {
     }
   
     const selectedImage = result.assets[0];
+  
+    // Obtener el blob de la imagen seleccionada
     const response = await fetch(selectedImage.uri);
     const blob = await response.blob();
   
+    // Extraer el nombre del archivo de la URI
     const filename = selectedImage.uri.substring(selectedImage.uri.lastIndexOf('/') + 1);
     const imageRef = ref(storage, `recipeImages/${filename}`);
   
     try {
+      // Subir el blob a Firebase Storage
       await uploadBytes(imageRef, blob);
+      
+      // Obtener la URL de descarga de la imagen
       const url = await getDownloadURL(imageRef);
+      
+      // Establecer la URL de la imagen en el estado o donde lo necesites
       setRecipeImage(url);
+      
+      Alert.alert('Éxito', 'La imagen se subió correctamente.');
     } catch (error) {
       console.error('Error al subir la imagen:', error);
       Alert.alert('Error', 'No se pudo subir la imagen.');
     }
   };
-  
 
   // Save the recipe and the image URL to Firestore
   const saveRecipe = async () => {
@@ -99,34 +96,50 @@ const CreateRecipeScreen = () => {
       Alert.alert('Error', 'Debes estar autenticado para guardar una receta.');
       return;
     }
-
-    const recipeData = {
-      recipeName,
-      recipeImage,
-      ingredients,
-      preparation,
-      dietType,
-      glutenFree,
-      vegetarian,
-      prepTime,
-      servings,
-      category: selectedCategory,
-    };
-
-    try {
-      // Update the user's document in the 'users' collection
-      const userDocRef = doc(db, 'users', user.email);
-      await updateDoc(userDocRef, {
-        recipesCreate: [...(user.recipesCreate || []), recipeData],
-      });
-
-      Alert.alert(`Receta "${recipeName}" guardada con éxito.`);
-    } catch (error) {
-      console.error('Error al guardar la receta:', error);
-      Alert.alert('Error', 'No se pudo guardar la receta. Inténtalo de nuevo más tarde.');
+  
+    const userDocRef = doc(db, 'users', user.email);
+    const userDoc = await getDoc(userDocRef);
+  
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      const currentRecipes = userData.misRecetas || [];
+  
+      // Verificar cuántas recetas tiene el usuario
+      if (currentRecipes.length >= 10) {
+        Alert.alert('Error', 'Ya has alcanzado el límite de 10 recetas por mes.');
+        return;
+      }
+  
+      const recipeData = {
+        id: generateId(),
+        recipeName,
+        recipeImage,
+        ingredients,
+        preparation,
+        dietType,
+        glutenFree,
+        vegetarian,
+        prepTime,
+        servings,
+        category: selectedCategory,
+      };
+  
+      try {
+        // Guardar la receta en el documento del usuario en Firestore
+        await setDoc(userDocRef, {
+          misRecetas: [...currentRecipes, recipeData],
+        }, { merge: true }); 
+  
+        Alert.alert(`Receta "${recipeName}" guardada con éxito.`);
+      } catch (error) {
+        console.error('Error al guardar la receta:', error);
+        Alert.alert('Error', 'No se pudo guardar la receta. Inténtalo de nuevo más tarde.');
+      }
+    } else {
+      Alert.alert('Error', 'No se encontraron datos para este usuario.');
     }
   };
-
+    
   const handleAddIngredient = () => {
     setIngredients([...ingredients, '']);
   };
@@ -210,7 +223,7 @@ const CreateRecipeScreen = () => {
                 style={styles.inputDetail}
                 onValueChange={(itemValue) => setSelectedCategory(itemValue)}
               >
-                <Picker.Item label="Seleccione una categoría" value="" />
+                <Picker.Item label="Vino" value="" />
                 {categories.map((category, index) => (
                   <Picker.Item key={index} label={category} value={category} />
                 ))}
@@ -246,13 +259,13 @@ const CreateRecipeScreen = () => {
               {ingredients.map((ingredient, index) => (
                 <View key={index} style={styles.ingredientRow}>
                   <TextInput
-                    style={styles.textArea}
-                    placeholder={`Ingrediente ${index + 1}`}
+                    style={styles.ingredientInput}
+                    placeholder="Ingrediente"
                     value={ingredient}
                     onChangeText={(text) => handleIngredientChange(text, index)}
                   />
                   <TouchableOpacity onPress={() => handleRemoveIngredient(index)}>
-                    <Icon name="trash-outline" size={24} color="#FF0000" />
+                    <Text style={styles.removeIngredientText}>X</Text>
                   </TouchableOpacity>
                 </View>
               ))}
@@ -265,15 +278,15 @@ const CreateRecipeScreen = () => {
           {activeTab === 'preparation' && (
             <View style={styles.preparationContainer}>
               {preparation.map((step, index) => (
-                <View key={index} style={styles.preparationRow}>
+                <View key={index} style={styles.stepRow}>
                   <TextInput
-                    style={styles.textArea}
-                    placeholder={`Paso ${index + 1}`}
+                    style={styles.stepInput}
+                    placeholder="Paso de preparación"
                     value={step}
                     onChangeText={(text) => handleStepChange(text, index)}
                   />
                   <TouchableOpacity onPress={() => handleRemoveStep(index)}>
-                    <Icon name="trash-outline" size={24} color="#FF0000" />
+                    <Text style={styles.removeStepText}>X</Text>
                   </TouchableOpacity>
                 </View>
               ))}
@@ -285,33 +298,21 @@ const CreateRecipeScreen = () => {
 
           {activeTab === 'diettype' && (
             <View style={styles.dietTypeContainer}>
-              <Text style={styles.label}>¿Es vegana?</Text>
-              <Picker
-                selectedValue={dietType}
-                style={styles.picker}
-                onValueChange={(itemValue) => setDietType(itemValue)}
-              >
-                <Picker.Item label="Sí" value="vegan" />
-                <Picker.Item label="No" value="no vegan" />
-              </Picker>
-              <Text style={styles.label}>¿Es libre de gluten?</Text>
-              <Picker
-                selectedValue={glutenFree}
-                style={styles.picker}
-                onValueChange={(itemValue) => setGlutenFree(itemValue)}
-              >
-                <Picker.Item label="Sí" value="yes" />
-                <Picker.Item label="No" value="no" />
-              </Picker>
-              <Text style={styles.label}>¿Es vegetariana?</Text>
-              <Picker
-                selectedValue={vegetarian}
-                style={styles.picker}
-                onValueChange={(itemValue) => setVegetarian(itemValue)}
-              >
-                <Picker.Item label="Sí" value="yes" />
-                <Picker.Item label="No" value="no" />
-              </Picker>
+              <Text style={styles.dietTypeText}>Tipo de dieta:</Text>
+              <View style={styles.dietTypeOptions}>
+                <TouchableOpacity onPress={() => setDietType('vegan')} style={[styles.dietTypeButton, dietType === 'vegan' && styles.selectedDietType]}>
+                  <Text>Vegano</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setDietType('vegetarian')} style={[styles.dietTypeButton, dietType === 'vegetarian' && styles.selectedDietType]}>
+                  <Text>Vegetariano</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setDietType('glutenFree')} style={[styles.dietTypeButton, dietType === 'glutenFree' && styles.selectedDietType]}>
+                  <Text>Sin TACC</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setDietType('dairyFree')} style={[styles.dietTypeButton, dietType === 'dairyFree' && styles.selectedDietType]}>
+                  <Text>Sin Lácteos</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           )}
 
--- src/screens/MyRecipes.js
+++ src/screens/MyRecipes.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/native';
 import Header from '../components/Headers/Header';
-import { collection, getDoc, doc } from 'firebase/firestore'; 
+import { doc, getDoc, setDoc } from 'firebase/firestore'; 
 import { auth, db } from '../../firebase/firebase-config';
 
 const MyRecipes = () => {
@@ -22,8 +22,10 @@ const MyRecipes = () => {
           
           if (userDoc.exists()) {
             const userData = userDoc.data();
-            if (userData && userData.recipesCreate) {
-              setMyRecipes(userData.recipesCreate);
+            if (userData && userData.misRecetas) {
+              setMyRecipes(userData.misRecetas); 
+            } else {
+              Alert.alert('Error', 'No tienes recetas creadas.');
             }
           } else {
             Alert.alert('Error', 'No se encontraron datos para este usuario.');
@@ -35,36 +37,96 @@ const MyRecipes = () => {
       } else {
         Alert.alert('Error', 'Debes estar autenticado para ver tus recetas.');
       }
-      setLoading(false);
+      setLoading(false); 
     };
 
     fetchRecipes();
   }, []);
 
+  const handleDeleteRecipe = async (recipeId) => {
+    const user = auth.currentUser;
+    if (user) {
+      try {
+        const userDocRef = doc(db, 'users', user.email);
+        const userDoc = await getDoc(userDocRef);
+        
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          // Filtramos las recetas para eliminar la receta con el id correspondiente
+          const updatedRecipes = userData.misRecetas.filter(recipe => recipe.id !== recipeId);
+  
+          // Actualizamos el documento del usuario con las recetas filtradas
+          await setDoc(userDocRef, { misRecetas: updatedRecipes });
+  
+          // Actualizamos el estado local para reflejar el cambio
+          setMyRecipes(updatedRecipes);
+          Alert.alert('Éxito', 'Receta eliminada correctamente.');
+        } else {
+          Alert.alert('Error', 'No se encontraron datos para este usuario.');
+        }
+      } catch (error) {
+        console.error('Error al eliminar receta:', error);
+        Alert.alert('Error', 'No se pudo eliminar la receta. Inténtalo de nuevo más tarde.');
+      }
+    } else {
+      Alert.alert('Error', 'Debes estar autenticado para eliminar recetas.');
+    }
+  };
+  
+  const confirmDelete = (recipeId) => {
+    Alert.alert(
+      'Confirmar eliminación',
+      '¿Estás seguro de que deseas eliminar esta receta?',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        { text: 'Eliminar', onPress: () => handleDeleteRecipe(recipeId) },
+      ],
+      { cancelable: false }
+    );
+  };
+
   const renderRecipe = ({ item }) => (
-    <TouchableOpacity
-      style={styles.recipeCard}
-      onPress={() => navigation.navigate('RecipeScreen', { recipeId: item.id })}
-    >
-      <Image source={{ uri: item.recipeImage }} style={styles.recipeImage} />
+    <View style={styles.recipeCard}>
+      {item.recipeImage ? (
+        <Image source={{ uri: item.recipeImage }} style={styles.recipeImage} />
+      ) : (
+        <View style={styles.placeholderImage}>
+          <Text style={styles.placeholderText}>Sin Imagen</Text>
+        </View>
+      )}
       <View style={styles.recipeInfo}>
-        <Text style={styles.recipeName}>{item.recipeName}</Text>
-        <Text style={styles.recipeCategory}>{item.category}</Text>
+        <Text style={styles.recipeName}>{item.recipeName || 'Receta sin nombre'}</Text>
+        <Text style={styles.recipeCategory}>{item.category || 'Sin categoría'}</Text>
+        <Text style={styles.recipeDetail}>Tipo de dieta: {item.dietType || 'No especificado'}</Text>
+        <Text style={styles.recipeDetail}>Sin gluten: {item.glutenFree || 'No especificado'}</Text>
+        <Text style={styles.recipeDetail}>Porciones: {item.servings || 'No especificado'}</Text>
+        <Text style={styles.recipeDetail}>Tiempo de preparación: {item.prepTime || 'No especificado'} minutos</Text>
+        <TouchableOpacity 
+          style={styles.deleteButton}
+          onPress={() => confirmDelete(item.id)}
+        >
+          <Text style={styles.deleteButtonText}>Eliminar</Text>
+        </TouchableOpacity>
       </View>
-    </TouchableOpacity>
+    </View>
   );
+  
 
   return (
     <View style={styles.container}>
       <Header />
 
       {loading ? (
-        <Text>Cargando recetas...</Text>
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#f77f00" />
+          <Text style={styles.loadingText}>Cargando recetas...</Text>
+        </View>
       ) : myRecipes.length > 0 ? (
         <FlatList
           data={myRecipes}
           renderItem={renderRecipe}
-          keyExtractor={(item) => item.id} 
+          keyExtractor={(item) => item.id ? item.id.toString() : Math.random().toString()} 
+          contentContainerStyle={styles.flatListContainer}
         />
       ) : (
         <View style={styles.emptyContainer}>
@@ -90,20 +152,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#fafafa',
   },
   recipeCard: {
-    flexDirection: 'row',
-    alignItems: 'center',
     marginBottom: 20,
     borderWidth: 1,
     borderColor: '#ddd',
     borderRadius: 10,
     padding: 10,
     backgroundColor: '#fff',
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3.84,
   },
   recipeImage: {
-    width: 70,
-    height: 70,
+    width: '100%',
+    height: 150,
     borderRadius: 10,
-    marginRight: 15,
+    marginBottom: 10,
+  },
+  placeholderImage: {
+    width: '100%',
+    height: 150,
+    borderRadius: 10,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+  },
+  placeholderText: {
+    color: '#aaa',
+    fontSize: 12,
   },
   recipeInfo: {
     flex: 1,
@@ -115,6 +193,27 @@ const styles = StyleSheet.create({
   },
   recipeCategory: {
     fontSize: 14,
+    color: '#666',
+  },
+  deleteButton: {
+    marginTop: 10,
+    backgroundColor: '#f77f00',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  deleteButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingText: {
+    marginTop: 10,
+    fontSize: 16,
     color: '#666',
   },
   emptyContainer: {
@@ -142,6 +241,9 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 3.84,
+  },
+  flatListContainer: {
+    paddingBottom: 80,
   },
 });
 
