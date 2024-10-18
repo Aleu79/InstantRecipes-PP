acdacd889c3758bdcff427897201e3ee08f582d1
Aleu79 <aleeaguero79@gmail.com>
2024-10-17 23:22:53 -0300

    correcciones 3.4

--- src/screens/RecipeScreen.js
+++ src/screens/RecipeScreen.js
@@ -15,7 +15,7 @@ const RecipeScreen = ({ route }) => {
 
   useEffect(() => {
     const checkSavedStatus = async () => {
-      let userEmail = await AsyncStorage.getItem('userEmail');
+      const userEmail = await AsyncStorage.getItem('userEmail');
       if (!userEmail) return;
 
       const userDoc = doc(db, 'users', userEmail);
@@ -33,7 +33,7 @@ const RecipeScreen = ({ route }) => {
 
   const handleRecipeSaveToggle = async () => {
     try {
-      let userEmail = await AsyncStorage.getItem('userEmail');
+      const userEmail = await AsyncStorage.getItem('userEmail');
       if (!userEmail) {
         Alert.alert('Error', 'No se pudo obtener el correo del usuario.');
         return;
@@ -99,7 +99,7 @@ const RecipeScreen = ({ route }) => {
   }
 
   const preparationSteps = recipe.instructions ? recipe.instructions.split('. ') : [];
-
+  
   const imageOpacity = scrollY.interpolate({
     inputRange: [0, 300],
     outputRange: [1, 0],
@@ -111,6 +111,7 @@ const RecipeScreen = ({ route }) => {
     outputRange: [0, -150],
     extrapolate: 'clamp',
   });
+
   return (
     <>
       {/* Header fijo */}
@@ -151,80 +152,86 @@ const RecipeScreen = ({ route }) => {
 
           <View style={styles.dietContainer}>
             {recipe.vegan && (
-              <View style={styles.dietDetail}>
-                <MaterialCommunityIcons name="leaf" size={24} color="green" />
-                <Text style={styles.dietText}>Vegano</Text>
-              </View>
+              <DietDetail iconName="leaf" color="green" text="Vegano" />
             )}
             {recipe.glutenFree && (
-              <View style={styles.dietDetail}>
-                <MaterialCommunityIcons name="bread-slice" size={24} color="orange" />
-                <Text style={styles.dietText}>Sin Gluten</Text>
-              </View>
+              <DietDetail iconName="bread-slice" color="orange" text="Sin Gluten" />
             )}
             {recipe.vegetarian && (
-              <View style={styles.dietDetail}>
-                <MaterialCommunityIcons name="food-apple" size={24} color="red" />
-                <Text style={styles.dietText}>Vegetariano</Text>
-              </View>
+              <DietDetail iconName="food-apple" color="red" text="Vegetariano" />
             )}
             {recipe.dairyFree && (
-              <View style={styles.dietDetail}>
-                <MaterialCommunityIcons name="glass-pint-outline" size={24} color="grey" />
-                <Text style={styles.dietText}>Sin lácteos</Text>
-              </View>
+              <DietDetail iconName="glass-pint-outline" color="grey" text="Sin lácteos" />
             )}
           </View>
 
           <View style={styles.separator}></View>
 
           <View style={styles.tabsContainer}>
-            <TouchableOpacity
-              style={[styles.tabButton, activeTab === 'ingredients' ? styles.activeTab : styles.inactiveTab]}
-              onPress={() => setActiveTab('ingredients')}
-            >
-              <Text style={[styles.tabText, activeTab === 'ingredients' && styles.activeTabText]}>Ingredientes</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.tabButton, activeTab === 'preparation' ? styles.activeTab : styles.inactiveTab]}
-              onPress={() => setActiveTab('preparation')}
-            >
-              <Text style={[styles.tabText, activeTab === 'preparation' && styles.activeTabText]}>Preparación</Text>
-            </TouchableOpacity>
+            <TabButton isActive={activeTab === 'ingredients'} onPress={() => setActiveTab('ingredients')} text="Ingredientes" />
+            <TabButton isActive={activeTab === 'preparation'} onPress={() => setActiveTab('preparation')} text="Preparación" />
           </View>
 
           {activeTab === 'ingredients' ? (
-            <View style={styles.ingredientsContainer}>
-              {recipe.ingredients.length > 0 ? (
-                recipe.ingredients.map((ingredient, index) => (
-                  <View key={index} style={styles.ingredientWrapper}>
-                    <Image source={{ uri: `https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}` }} style={styles.ingredientImage} />
-                    <View>
-                      <Text style={styles.ingredientName}>{capitalizeFirstLetter(ingredient.originalName)}</Text>
-                      <Text style={styles.ingredientAmount}>{ingredient.amount} {ingredient.unit}</Text>
-                    </View>
-                  </View>
-                ))
-              ) : (
-                <Text>No hay ingredientes disponibles.</Text>
-              )}
-            </View>
+            <IngredientsList ingredients={recipe.ingredients || []} />
           ) : (
-            <View style={styles.preparationContainer}>
-              {preparationSteps.length > 0 ? (
-                preparationSteps.map((step, index) => (
-                  <Text key={index} style={styles.preparationStep}>{step}</Text>
-                ))
-              ) : (
-                <Text>No hay instrucciones disponibles.</Text>
-              )}
-            </View>
+            <PreparationList preparationSteps={preparationSteps} />
           )}
         </View>
       </Animated.ScrollView>
     </>
   );
 };
+
+const DietDetail = ({ iconName, color, text }) => (
+  <View style={styles.dietDetail}>
+    <MaterialCommunityIcons name={iconName} size={24} color={color} />
+    <Text style={styles.dietText}>{text}</Text>
+  </View>
+);
+
+const TabButton = ({ isActive, onPress, text }) => (
+  <TouchableOpacity
+    style={[styles.tabButton, isActive ? styles.activeTab : styles.inactiveTab]}
+    onPress={onPress}
+  >
+    <Text style={[styles.tabText, isActive && styles.activeTabText]}>{text}</Text>
+  </TouchableOpacity>
+);
+
+const IngredientsList = ({ ingredients }) => (
+  <View style={styles.ingredientsContainer}>
+    {ingredients.length > 0 ? (
+      ingredients.map((ingredient, index) => (
+        <IngredientItem key={index} ingredient={ingredient} />
+      ))
+    ) : (
+      <Text>No hay ingredientes disponibles.</Text>
+    )}
+  </View>
+);
+
+const IngredientItem = ({ ingredient }) => (
+  <View style={styles.ingredientWrapper}>
+    <Image source={{ uri: `https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`} } style={styles.ingredientImage} />
+    <View>
+      <Text style={styles.ingredientName}>{capitalizeFirstLetter(ingredient.originalName)}</Text>
+      <Text style={styles.ingredientAmount}>{ingredient.amount} {ingredient.unit}</Text>
+    </View>
+  </View>
+);
+
+const PreparationList = ({ preparationSteps }) => (
+  <View style={styles.preparationContainer}>
+    {preparationSteps.length > 0 ? (
+      preparationSteps.map((step, index) => (
+        <Text key={index} style={styles.preparationStep}>{step}</Text>
+      ))
+    ) : (
+      <Text>No hay instrucciones disponibles.</Text>
+    )}
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -238,102 +245,68 @@ const styles = StyleSheet.create({
   recipeImage: {
     width: '100%',
     height: '100%',
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-  },
-  headerContainer: {
-    position: 'absolute',
-    top: 40,
-    left: 0,
-    right: 0,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
-    zIndex: 10,
-    borderRadius: 25,
-    padding: 10,
+    resizeMode: 'cover',
   },
   contentContainer: {
-    backgroundColor: '#fff',
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-    marginTop: -30,
     padding: 20,
   },
   recipeName: {
-    fontSize: 28,
+    fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 15,
-    marginTop: 25,
-    textAlign: 'left',
+    marginBottom: 10,
   },
   recipeDetails: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginBottom: 20,
-    marginTop: 20,
+    justifyContent: 'space-between',
+    marginBottom: 10,
   },
   infoContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
   },
   infoText: {
-    fontSize: 14,
+    marginLeft: 5,
     color: '#888',
-    marginTop: 5,
   },
   dietContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginBottom: 20,
-    marginTop: 10,
+    marginBottom: 10,
   },
   dietDetail: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginHorizontal: 10,
-    borderRadius: 20,
-    padding: 10,
-    backgroundColor: '#f2f2f2',
+    marginRight: 10,
   },
   dietText: {
-    fontSize: 14,
-    color: '#888',
     marginLeft: 5,
+    color: '#888',
   },
   separator: {
-    height: 2,
-    backgroundColor: '#f5f5f5',
-    width: '90%',
-    alignSelf: 'center',
-    marginVertical: 20,
+    height: 1,
+    backgroundColor: '#eee',
+    marginVertical: 10,
   },
   tabsContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    marginBottom: 20,
-    borderRadius: 25,
-    backgroundColor: '#f5f5f5',
-    padding: 5,
+    justifyContent: 'space-between',
   },
   tabButton: {
+    paddingVertical: 10,
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 10,
-    borderRadius: 25,
   },
   activeTab: {
-    backgroundColor: '#e0e0e0',
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
   },
   inactiveTab: {
-    backgroundColor: 'transparent',
+    borderBottomWidth: 0,
   },
   tabText: {
     fontSize: 16,
-    fontWeight: '500',
+    color: '#888',
   },
   activeTabText: {
-    fontWeight: 'bold',
     color: '#000',
   },
   ingredientsContainer: {
@@ -342,44 +315,38 @@ const styles = StyleSheet.create({
   ingredientWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 15,
+    marginBottom: 10,
   },
   ingredientImage: {
     width: 50,
     height: 50,
+    borderRadius: 25,
     marginRight: 10,
-    resizeMode: 'contain',
   },
   ingredientName: {
     fontSize: 16,
     fontWeight: 'bold',
   },
   ingredientAmount: {
-    fontSize: 14,
     color: '#888',
   },
   preparationContainer: {
     marginTop: 10,
   },
-  stepWrapper: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    marginBottom: 15,
-  },
-  stepNumber: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginRight: 10,
-    color: '#333',
-  },
-  stepText: {
+  preparationStep: {
     fontSize: 16,
-    color: '#333',
+    marginBottom: 5,
   },
   loadingContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    backgroundColor: '#333',
   },
   backIcon: {
     padding: 10,
@@ -390,4 +357,3 @@ const styles = StyleSheet.create({
 });
 
 export default RecipeScreen;
-
